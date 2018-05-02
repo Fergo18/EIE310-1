19b3a399a7a893d672a2a110a44e364de213afc5
Fergo18 <37382180+Fergo18@users.noreply.github.com>
2018-05-02 16:53:35 -0300

    Add files via upload

---
+++ Ejercicio5.m
@@ -0,0 +1,7 @@
+w = 0: (pi/200):pi;
+X = exp(j*w)+2+3*exp(-j*w)+4*exp(-j*2*w)+5*exp(-j*3*w);
+subplot 331;plot(w/pi,abs(X));grid
+xlabel ('w/Pi [Rad/m]'); title ('Magnitud')
+subplot 332;plot (w/pi,angle(X)/pi);grid
+subplot 332;plot (w/pi,unwrap(angle(X))/pi);grid
+xlabel('w/Pi [Rad/m]');title('Angulo/Pi [rad]')
