83230f6c52a1578e677729f652ad4aa6b9be242c
Fergo18 <37382180+Fergo18@users.noreply.github.com>
2018-07-04 17:57:06 -0300

    Add files via upload

---
+++ Ayudantia4_Ejercicio1.m
@@ -0,0 +1,23 @@
+load h.txt
+subplot 221;
+plot(h, '.'); grid
+xlabel('n');
+title('h(n)');
+f = 0 : 100 : 200000;
+M = abs(freqz(h,1,f,176400));
+subplot 222;
+plot(h, '.'); grid
+xlabel('n');
+title('h(n)');
+f = 0 : 100 : 180000;
+M = abs(freqz(h,1,f,176400));
+subplot 223;
+semilogy(f/1000, M); grid;
+axis([0 200 0.000001 10]);
+xlabel('f[kHz]');
+title('Mag[H(f)]'); 
+subplot 224;
+semilogy(f/1000, M); grid;
+axis([0 180 0.000001 10]);
+xlabel('f[kHz]');
+title('Mag[H(f)]'); 
